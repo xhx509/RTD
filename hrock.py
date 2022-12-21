de15426a73a28cf7705da8004348a4cef18b12af
huanxin <xhx509@users.noreply.github.com>
2022-12-21 10:33:58 -0500

    Add files via upload

---
+++ hrock.py
@@ -0,0 +1,29 @@
+import rockBlock
+import time 
+from rockBlock import rockBlockProtocol
+'''    
+    def main(self,ports):
+        print ports
+'''   
+class MoExample(rockBlockProtocol):
+    def __init__(self, ports,message):
+        
+    
+        rb = rockBlock.rockBlock(ports, self)
+        time.sleep(2)
+        
+        rb.sendMessage(message)      
+        
+        rb.close()
+        
+    def rockBlockTxStarted(self):
+        print ("rockBlockTxStarted")
+        
+    def rockBlockTxFailed(self):
+        print ("rockBlockTxFailed")
+        
+    def rockBlockTxSuccess(self,momsn):
+        print ("rockBlockTxSuccess " + str(momsn))
+        
+if __name__ == '__main__':
+    MoExample().main()
---
+++ t_rock.py
@@ -0,0 +1,6 @@
+#please change the ports
+import hrock
+from hrock import MoExample
+ports='/dev/ttyUSB0'
+message='1111111111111111'
+MoExample(ports,message)
